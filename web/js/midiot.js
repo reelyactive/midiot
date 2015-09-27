82aaccb02bb757170cd797e5f734f73b498ad9a9
Jeffrey Dungen <jeff@reelyactive.com>
2015-09-26 23:52:36 -0400

    Added websockets

--- web/js/midiot.js
+++ web/js/midiot.js
@@ -1,6 +1,16 @@
-angular.module('midiot', [ 'ui.bootstrap' ])
+DEFAULT_SOCKET_URL = 'http://localhost:3006';
+
+
+angular.module('midiot', [ 'ui.bootstrap', 'btford.socket-io' ])
+
+  // Socket.io factory
+  .factory('Socket', function(socketFactory) {
+    return socketFactory( { ioSocket: io.connect(DEFAULT_SOCKET_URL) } );
+  })
 
   // Midi controller
-  .controller('MidiCtrl', function($scope, $http, $interval, $window) {
-
+  .controller('MidiCtrl', function($scope, $http, $interval, Socket) {
+    Socket.on('event', function(event) {
+      $scope.event = JSON.stringify(event);
+    });
   });
